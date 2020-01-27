f8053d0788c2ed5f01a9d53dd5ebead1def84323
Murtaza <Murtaza_taloo@hotmail.com>
2020-01-28 10:08:32 +1100

    removed what wasn't needed from the cartpage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -65,15 +65,3 @@ const mapDispatchProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchProps)(CartPage);
-
-// <Container>
-//   <Row>
-//     <Col>1 of 2</Col>
-//     <Col>2 of 2</Col>
-//   </Row>
-//   <Row>
-//     <Col>1 of 3</Col>
-//     <Col>2 of 3</Col>
-//     <Col>3 of 3</Col>
-//   </Row>
-// </Container>;
