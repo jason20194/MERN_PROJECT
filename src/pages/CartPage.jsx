028b64ecb136e11e7e0921f1c1e3fc5d5fca9742
Murtaza <Murtaza_taloo@hotmail.com>
2020-01-28 11:48:02 +1100

    the cart now calculates the total and successfully removes the items from cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -14,42 +14,48 @@ class CartPage extends Component {
   render() {
     const data = this.props.products;
     const totalCart = data.reduce((a, b) => {
-      return parseFloat(a.price) + parseFloat(b.price);
-    });
-    return data
-      ? data.map((product, index) => {
-          return (
-            <div>
-              <Container>
-                <Row>
-                  <Col sm={8}>
-                    <div key={index} className="products-on-cart">
-                      <h1>titile : {product.title}</h1>
-                      <p>id: {product._id}</p>
-                      <p>description:{product.description}</p>
-                      <p>${product.price}</p>
-                    </div>
-                  </Col>
-                  <Col sm={4}>
-                    <p>${product.price}</p>
+      return a + parseFloat(b.price);
+    }, 0);
 
-                    <RemoveFromCart
-                      product={product}
-                      itemRemovedFromCart={this.itemRemovedFromCart}
-                    />
-                  </Col>
-                </Row>
-              </Container>
-              {/* <Container>
-                <Row>
-                  <Col sm={8}>CHECKOUT</Col>
-                  <Col sm={4}>Total Cart Value = {totalCart}</Col>
-                </Row>
-              </Container> */}
-            </div>
-          );
-        })
-      : null;
+    console.log(totalCart);
+
+    return (
+      <div>
+        {data
+          ? data.map((product, index) => {
+              return (
+                <Container>
+                  <Row>
+                    <Col sm={8}>
+                      <div key={index} className="products-on-cart">
+                        <h1>titile : {product.title}</h1>
+                        {/* <p>id: {product._id}</p> */}
+                        <p>description:{product.description}</p>
+                        {/* <p>${product.price}</p> */}
+                      </div>
+                    </Col>
+                    <Col sm={4}>
+                      <p>${product.price}</p>
+
+                      <RemoveFromCart
+                        product={product}
+                        itemRemovedFromCart={this.itemRemovedFromCart}
+                      />
+                    </Col>
+                  </Row>
+                  <hr />
+                </Container>
+              );
+            })
+          : null}
+        <Container>
+          <Row>
+            <Col sm={8}>CHECKOUT</Col>
+            <Col sm={4}>Total Cart Value = {totalCart}</Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
