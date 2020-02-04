cd99ee618fe3d6590a644ee275d4172b4afc49c1
Marco Lo <marco1993@live.com>
2020-02-04 16:19:48 +1100

    cleaned up files
    deleted about us flex box test

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
-import "../components/FlexboxTest.css";
-import Card from "react-bootstrap/Card";
 
 class AboutUs extends Component {
   render() {
     return (
-      <div className="main-body">
-        <div className="left-side"></div>
-        <div className="right-side">
-          <div className="product-box">
-            <h1>This is the product</h1>
-            <p>a little bit about the product</p>
-          </div>
-          <div className="description-box">
-            <h1>This is the description</h1>
-            <p>a little bit about the description</p>
-          </div>
-        </div>
+      <div>
+        <h1>Hi from About Us</h1>
       </div>
     );
   }
