1c4cb0f3102036b12a910ce522afaf4ac9b50924
Murtaza <Murtaza_taloo@hotmail.com>
2020-02-04 20:07:55 +1100

    things working fine after mergin json's pull req

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,36 +5,6 @@ import Logout from "./Logout";
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-<<<<<<< HEAD
-  <a className="navbar-brand" href="/">
-  <img id="brand-image" alt="Website Logo" src={require('../images/medicinepower.png')} />
-  </a>
-  <a className="navbar-brand">Medicine Power</a>
-  <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto nav-fill w-100">
-      <li className="nav-item">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/all_products">Products</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/about_us">About Us</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/contact">Contact</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/cart">Cart</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-  )
-=======
       <a className="navbar-brand" href="/">
         <img
           id="brand-image"
@@ -44,7 +14,7 @@ export default function NavBar() {
       </a>
       <a className="navbar-brand">Medicine Power</a>
       <button
-        className="navbar-toggler"
+        className="navbar-toggler collapsed"
         type="button"
         data-toggle="collapse"
         data-target="#navbarSupportedContent"
@@ -55,8 +25,8 @@ export default function NavBar() {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
+        <ul className="navbar-nav mr-auto nav-fill w-100">
+          <li className="nav-item">
             <a className="nav-link" href="/">
               Home <span className="sr-only">(current)</span>
             </a>
@@ -88,5 +58,4 @@ export default function NavBar() {
       </div>
     </nav>
   );
->>>>>>> 9647a0b473d3935afd7118a8ab90f53525cf334f
 }
