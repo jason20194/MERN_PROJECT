cc3dd7748a78712ec379aad8b030d2937a30d358
Murtaza <Murtaza_taloo@hotmail.com>
2020-02-06 02:07:39 +1100

    added a few cypress tests

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -20,5 +20,34 @@ describe("App E2E", () => {
     cy.get(".email").type("Customer@gmail.com");
     cy.get(".message").type("this is the message for testing");
     cy.get(".submit-button").click();
+    cy.wait(1000);
+  });
+
+  it("clicking Products page", () => {
+    cy.get("nav")
+      .get(".all-products")
+      .click();
+  });
+
+  it("Viewing a Product", () => {
+    cy.get("nav")
+      .get(".all-products")
+      .get(".each-product")
+      // fails because its multiple divs
+      .get(".product-details")
+      .click();
+  });
+
+  it("admin login", () => {
+    cy.visit("http://localhost:3000/admin/login");
+    cy.get(".username").type("admin@gmail.com");
+    cy.get(".password").type("password");
+    cy.get(".admin-login").click();
+  });
+
+  // this doesnt run because it doesnt have a token
+  it("create listing", () => {
+    cy.visit("http://localhost:3000/new_listing");
+    // cy.get(".create-listing").click();
   });
 });
