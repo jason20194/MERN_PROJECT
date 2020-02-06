984908af867e386424af7d0fa1015b303ad9d8dd
Jason Liu <jasonliu20194@gmail.com>
2020-02-06 13:48:56 +1100

    added cypress testing to new and edit forms

---
+++ cypress/integration/create_listing_spec.js
@@ -0,0 +1,25 @@
+beforeEach(() => {
+  localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsIm9uZU1vcmUiOiJ5ZXMiLCJpYXQiOjE1ODA5NTY1MDQsImV4cCI6MTU4MDk2MDEwNH0.D9ANUjIRSdKWslFb3F2tZnkLvgFculdYU0xBeVWu4K4")
+})
+
+
+describe('Create Listing', function() {
+    it("Clicks the create button and makes a new item and the item should show in the listings", function(){
+      cy.visit('http://localhost:3000/admin')
+      cy.contains("button", "Create Listing").click();
+      cy.get("input[name=title]").type("Cypress Test");
+      cy.get("input[name=price]").type("150");
+      cy.get('[type="checkbox"]').check();
+      cy.get("textarea[name=description]").type("Testing to see if description box works in Cypress")
+      cy.contains("button", "Create Product").click();
+      cy.wait(1000)
+      
+
+    })
+  })
+
+      // cy.visit('http://localhost:3000/admin')
+  // cy.get("input[name=username").type("admin@gmail.com");
+  // cy.get("input[name=password").type("password")
+  // cy.contains("button", "Log In").click();
+  // cy.wait(1000)
