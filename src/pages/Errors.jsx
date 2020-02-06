f9275ced782c3f9c7c4dceb682fceeb7ff5253f9
Jason Liu <jasonliu20194@gmail.com>
2020-02-06 16:27:26 +1100

    added a thank you and error page

---
+++ src/pages/Errors.jsx
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+export class Errors extends Component {
+    render() {
+        return (
+            <div className="error">
+                <h1>SORRY ABOUT THAT</h1>
+                <p>The request you have been looking for hasnt been found or isnt available right now. We apologize for the inconvenience.</p>
+            </div>
+        )
+    }
+}
+
+export default Errors
--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -28,7 +28,7 @@ class ThankYou extends Component {
     return (
       <div className="thank-you">
         <h1>THANK YOU</h1>
-        <p>Thanks for your purchase! We look forward to serving you again! :)</p>
+        <p>Thanks for your purchase! We look forward to serving you again!</p>
         {/* {order
           ? order !== null && (
               <div className="font">
