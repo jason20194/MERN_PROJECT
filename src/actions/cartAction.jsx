e53e21ac4b9b123e52dd399e9f55232501345295
Murtaza <Murtaza_taloo@hotmail.com>
2020-02-06 15:47:29 +1100

    fixed the cart update on the thankyou page

--- src/actions/cartAction.jsx
+++ src/actions/cartAction.jsx
@@ -2,26 +2,9 @@ import { UPDATE_CART } from "./types";
 
 // the functions created here will be exported to the component that needs to use that function. those function will be adding a item and removing item from the cart
 
-// how will they get access to the data??
-// export const addItem = item => {
-//   return {
-//     type: ADD_ITEM,
-//     data: item
-//   };
-// };
-
-export const updateCart = (items) => {
+export const updateCart = items => {
   return {
     type: UPDATE_CART,
     data: items
-  }
-}
-
-// // are we going to make the api calls here or in reducers???
-
-// export const removeItem = item => {
-//   return {
-//     type: REMOVE_ITEM,
-//     data: item
-//   };
-// };
+  };
+};
